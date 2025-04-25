ebe1df62dfbfe81245bd5847d56440d1b2d75e6c
Nadim Hossain Molla <nadimhossainmolla@outlook.com>
2025-04-25 10:28:15 +0530

    test commit from new system

--- MatrimonyAPI/obj/Debug/net8.0/MatrimonyAPI.AssemblyInfo.cs
+++ MatrimonyAPI/obj/Debug/net8.0/MatrimonyAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MatrimonyAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+046314bc8f56cf9b41f9fedc4dce74de026448d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MatrimonyAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MatrimonyAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
